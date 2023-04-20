496ee983b3758c760915a38d4f75320d4f2e636d
Mahram Hossain <mhnazary2020@yahoo.com>
2023-04-21 00:34:54 +0430

    Update package.json

--- script/LocalStorage.js
+++ script/LocalStorage.js
@@ -1,46 +1,36 @@
 let formData = {
-    name: '',
-    email: '',
-    message: ''
-  };
-  
-  const inputName = document.getElementById('name');
-  const inputEmail = document.getElementById('email');
-  const inputMessage = document.getElementById('contact-descrip');
-  
-  for (let key in formData ) {
-    if (formData.hasOwnProperty(key)) {
-      switch (key) {
-        case 'name':
-          inputName.addEventListener('input', event => {
-            formData.name = event.target.value;
-            localStorage.setItem('formData', JSON.stringify(formData));
-          });
-          break;
-        case 'email':
-          inputEmail.addEventListener('input', event => {
-            formData.email = event.target.value;
-            localStorage.setItem('formData', JSON.stringify(formData));
-          });
-          break;
-        case 'message':
-          inputMessage.addEventListener('input', event => {
-            formData.message = event.target.value;
-            localStorage.setItem('formData', JSON.stringify(formData));
-          });
-          break;
-      }
-    }
-  }
-  
-  if (localStorage.getItem('formData')) {
-    formData = JSON.parse(localStorage.getItem('formData'));
-    inputName.value = formData.name;
-    inputEmail.value = formData.email;
-    inputMessage.value = formData.message;
-  }
-  
-  const form = document.getElementById('contact-form');
-  form.addEventListener('submit', event => {
-    localStorage.setItem('formData', JSON.stringify(formData));
-  });
+  name: '',
+  email: '',
+  message: '',
+};
+
+const inputName = document.getElementById('name');
+const inputEmail = document.getElementById('email');
+const inputMessage = document.getElementById('contact-descrip');
+
+inputName.addEventListener('input', (event) => {
+  formData.name = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+inputEmail.addEventListener('input', (event) => {
+  formData.email = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+inputMessage.addEventListener('input', (event) => {
+  formData.message = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+if (localStorage.getItem('formData')) {
+  formData = JSON.parse(localStorage.getItem('formData'));
+  inputName.value = formData.name;
+  inputEmail.value = formData.email;
+  inputMessage.value = formData.message;
+}
+
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', () => {
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
